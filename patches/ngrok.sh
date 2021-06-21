e5b91b641f52e2c63773ab11a9aa43f1753602cf
Yisus7u7 <64093255+Yisus7u7@users.noreply.github.com>
2021-06-20 23:12:07 -0500

    Add files via upload

---
+++ patches/ngrok.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+echo "Downloading ngrok..."
+case `dpkg --print-architecture` in
+aarch64)
+    architectureURL="arm64" ;;
+arm)
+    architectureURL="arm" ;;
+armhf)
+    architectureURL="armhf" ;;
+amd64)
+    architectureURL="amd64" ;;
+i*86)
+    architectureURL="i386" ;;
+x86_64)
+    architectureURL="amd64" ;;
+*)
+    echo "unknown or unsupported architecture"
+esac
+
+wget "https://github.com/tchelospy/NgrokTest/blob/master/ngrok-stable-linux-${architectureURL}.zip?raw=true" -O ngrok.zip
+unzip ngrok.zip
+rm ngrok.zip
