234903593925aa421918668696227e3b1f9302c0
Yisus7u7 <64093255+Yisus7u7@users.noreply.github.com>
2021-06-29 20:32:14 -0500

    Update pkgs_yisus_install.sh

--- pkgs_yisus_install.sh
+++ pkgs_yisus_install.sh
@@ -3,11 +3,11 @@ clear
 echo -e "\e[1;32m tanks for installing \e[1m"
 cd $HOME
 
-RELEASE="pkgs_yisus-v2021.2"
-PKG="2021.2"
+RELEASE="pkgs_yisus-v2021.4"
+PKG="2021.4"
 
-wget https://github.com/pkgs-yisus/pkgs.yisus.org/releases/download/pkgs_yisus-v2021.2/pkgs-keyring_2021.2_all.deb
+wget https://github.com/pkgs-termux/pkgs.org/releases/download/pkgs_yisus-v2021.4/pkgs-keyring_2021.4_all.deb
 
-apt install ./pkgs-keyring_2021.2_all.deb
+apt install ./pkgs-keyring_2021.4_all.deb
 
-rm $HOME/pkgs-keyring_2021.2_all.deb
+rm $HOME/pkgs-keyring_2021.4_all.deb
