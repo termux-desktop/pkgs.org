9ec750cc6f2c27985a4860af548913ec1d2eb11e
Yisus7u7 <jesuspixel5@gmail.com>
2021-09-27 15:20:42 -0500

    update

--- up-pkgs-db.sh
+++ up-pkgs-db.sh
@@ -7,3 +7,6 @@ cp mirrors/dists/pkgs/stable/binary-*/*.deb debs/
 
 echo "[*] Adding packages..."
 python ./termux-apt-repo.py $PKGS mirrors pkgs stable
+
+echo "[*] Clean up..."
+rm -rf ./debs/*.deb
